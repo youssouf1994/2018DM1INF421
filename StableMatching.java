b16c204e31914c384c357cd8b75344319e661b9d
youssouf1994 <youssouf1994@github.com>
2018-12-12 15:46:13 -0500

    Add the constructStableMatching function

---
+++ StableMatching.java
@@ -0,0 +1,12 @@
+
+class StableMatching implements StableMatchingInterface {
+
+  public int[][] constructStableMatching (
+    int[] menGroupCount,
+    int[] womenGroupCount,
+    int[][] menPrefs,
+    int[][] womenPrefs
+  ) {
+
+  }
+}
