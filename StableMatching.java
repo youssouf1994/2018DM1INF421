0c0353655310d0c5dc5c754c1706efe5f3bc60ab
youssouf1994 <youssouf1994@github.com>
2018-12-12 16:15:22 -0500

    Add initialization for variables

--- StableMatching.java
+++ StableMatching.java
@@ -7,9 +7,26 @@ class StableMatching implements StableMatchingInterface {
     int[][] menPrefs,
     int[][] womenPrefs
   ) {
-    int m = menPrefs.length;
-    int w = womenPrefs.length;
+    int m = menGroupCount.length;
+    int w = womenGroupCount.length;
+
+    int[] singleMenGroupCount = new int[m];
+    int singleMen = 0;
+    for (int i = 0; i < m; i++) {
+      singleMenGroupCount[i] = menGroupCount[i];
+      singleMen += singleMenGroupCount[i];
+    }
+
     int[][] M = new int[m][w];
+
+    while (singleMen > 0) {
+      int currentMenGroup = 0;
+      for (int i = 0; i < m; i++) {
+        if(singleMenGroupCount[i] > singleMenGroupCount[currentMenGroup]) {
+          currentMenGroup = i;
+        }
+      }
+    }
 
     return M;
   }
