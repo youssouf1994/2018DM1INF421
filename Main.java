980bee5cc440577dc6ebf13bb76f1e0420dc4925
youssouf1994 <youssouf1994@github.com>
2018-12-13 11:40:35 -0500

    Finalize the implementation

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main (String[] args)
     {
 	// The random seed must be fixed, so that the tests are the same for everyone
-	// and are reproducible. 
+	// and are reproducible.
 	new StableMatchingTest (
 	    new StableMatching (),
 	    System.out,
--- StableMatching.java
+++ StableMatching.java
@@ -46,24 +46,35 @@ class StableMatching implements StableMatchingInterface {
       }
     }
 
-
+    // The number of single men in every group.
     int[] singleMenGroupCount = new int[m];
+
+    // The number of all single men.
     int singleMen = 0;
     for (int i = 0; i < m; i++) {
       singleMenGroupCount[i] = menGroupCount[i];
       singleMen += singleMenGroupCount[i];
     }
 
+    // The number of single women in every group.
     int[] singleWomenGroupCount = new int[w];
     for (int j = 0; j < w; j++) {
       singleWomenGroupCount[j] = womenGroupCount[j];
     }
 
-    int[] lastPropWomenGroup = new int [m];
+    /*
+      For every group of men we store the most attractive women group
+      that the group of men does not propose to.
+      */
+    int[] mostUnproposedWomenGroup = new int [m];
     for (int i = 0; i < m; i++) {
-      lastPropWomenGroup[i] = 0;
+      mostUnproposedWomenGroup[i] = 0;
     }
-
+    /*
+      For every group of women we store all groups of men engaded to the women group.
+      We store every group in PriorityQueue to have a direct access of the least att-
+      ractive men for this group.
+      */
     ArrayList<PriorityQueue <Integer>> menGroupEngagedTo = new ArrayList<PriorityQueue <Integer>>(w);
     for (int j = 0; j < w; j++) {
       menGroupEngagedTo.add(new PriorityQueue<Integer> (m, new PrefComparator(invWomenPrefs[j])));
@@ -75,47 +86,26 @@ class StableMatching implements StableMatchingInterface {
         M[i][j] = 0;
       }
     }
-    // A boolean to check if a proposal conduct to an engagement or not
-    boolean engagement = true;
 
-    boolean done = true;
-
-    int currentMenGroup = 0;
+    LinkedList <Integer> singleMenGroupWithBigNumber = new LinkedList <Integer>();
 
     while (singleMen > 0) {
 
-      if (done) {
-        // Search for the group with maximum single men.
-        currentMenGroup = 0;
+      if (singleMenGroupWithBigNumber.isEmpty()) {
         for (int i = 0; i < m; i++) {
-          if (singleMenGroupCount[i] > singleMenGroupCount[currentMenGroup]) {
-            currentMenGroup = i;
+          if (singleMenGroupCount[i] > (singleMen /(2*m))) {
+            singleMenGroupWithBigNumber.add(i);
           }
         }
       }
 
-      done = false;
-      int currentWomenGroup = menPrefs[currentMenGroup][lastPropWomenGroup[currentMenGroup]];
+      while (!singleMenGroupWithBigNumber.isEmpty()) {
 
-      if (singleWomenGroupCount[currentWomenGroup] > 0) {
-        int a = singleWomenGroupCount[currentWomenGroup];
-        int b = singleMenGroupCount[currentMenGroup];
+        int currentMenGroup = singleMenGroupWithBigNumber.poll();
+        int currentWomenGroup = menPrefs[currentMenGroup][mostUnproposedWomenGroup[currentMenGroup]];
 
-        int c = (a > b) ? b : a;
-
-        if (M[currentMenGroup][currentWomenGroup] == 0) {
-          menGroupEngagedTo.get(currentWomenGroup).offer(currentMenGroup);
-        }
-        singleMen -= c;
-        singleMenGroupCount[currentMenGroup] -= c;
-        singleWomenGroupCount[currentWomenGroup] -= c;
-        M[currentMenGroup][currentWomenGroup] += c;
-      }
-      else {
-        Integer leastAttractiveMenGroup = menGroupEngagedTo.get(currentWomenGroup).peek();
-        assert(leastAttractiveMenGroup != null);
-        if (invWomenPrefs[currentWomenGroup][currentMenGroup] < invWomenPrefs[currentWomenGroup][leastAttractiveMenGroup]) {
-          int a = M[leastAttractiveMenGroup][currentWomenGroup];
+        if (singleWomenGroupCount[currentWomenGroup] > 0) {
+          int a = singleWomenGroupCount[currentWomenGroup];
           int b = singleMenGroupCount[currentMenGroup];
 
           int c = (a > b) ? b : a;
@@ -123,21 +113,38 @@ class StableMatching implements StableMatchingInterface {
           if (M[currentMenGroup][currentWomenGroup] == 0) {
             menGroupEngagedTo.get(currentWomenGroup).offer(currentMenGroup);
           }
+          singleMen -= c;
           singleMenGroupCount[currentMenGroup] -= c;
+          singleWomenGroupCount[currentWomenGroup] -= c;
           M[currentMenGroup][currentWomenGroup] += c;
-
-          singleMenGroupCount[leastAttractiveMenGroup] += c;
-          M[leastAttractiveMenGroup][currentWomenGroup] -= c;
-          if (M[leastAttractiveMenGroup][currentWomenGroup] == 0) {
-            menGroupEngagedTo.get(currentWomenGroup).remove();
-          }
         }
         else {
-          lastPropWomenGroup[currentMenGroup] += 1;
+          Integer leastAttractiveMenGroup = menGroupEngagedTo.get(currentWomenGroup).peek();
+          if (invWomenPrefs[currentWomenGroup][currentMenGroup] < invWomenPrefs[currentWomenGroup][leastAttractiveMenGroup]) {
+            int a = M[leastAttractiveMenGroup][currentWomenGroup];
+            int b = singleMenGroupCount[currentMenGroup];
+
+            int c = (a > b) ? b : a;
+
+            if (M[currentMenGroup][currentWomenGroup] == 0) {
+              menGroupEngagedTo.get(currentWomenGroup).offer(currentMenGroup);
+            }
+            singleMenGroupCount[currentMenGroup] -= c;
+            M[currentMenGroup][currentWomenGroup] += c;
+
+            singleMenGroupCount[leastAttractiveMenGroup] += c;
+            M[leastAttractiveMenGroup][currentWomenGroup] -= c;
+            if (M[leastAttractiveMenGroup][currentWomenGroup] == 0) {
+              menGroupEngagedTo.get(currentWomenGroup).remove();
+            }
+          }
+          else {
+            mostUnproposedWomenGroup[currentMenGroup] += 1;
+          }
         }
-      }
-      if (singleMenGroupCount[currentMenGroup] == 0) {
-        done = true;
+        if (singleMenGroupCount[currentMenGroup] > (singleMen /(2*m))) {
+          singleMenGroupWithBigNumber.add(currentMenGroup);
+        }
       }
     }
 
